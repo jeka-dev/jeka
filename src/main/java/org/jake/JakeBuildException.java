9b719f79001569f562e9b0a0b6d70129dde722e2
i19451 <i19451@WDD06320.resnp.sysnp.shared.fortis>
2014-07-18 15:34:58 +0200

    fix

---
+++ src/main/java/org/jake/JakeBuildException.java
@@ -0,0 +1,11 @@
+package org.jake;
+
+public class JakeBuildException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public JakeBuildException(String message) {
+		super(message);
+	} 
+
+}
