494577fce06da0c87a624c667d9790f16bd29eef
i19451 <i19451@WDD06320.resnp.sysnp.shared.fortis>
2014-09-17 17:51:43 +0200

    jakoco

--- build/spec/FullBuild.java
+++ build/spec/FullBuild.java
@@ -5,14 +5,6 @@ import org.jake.java.test.junit.JakeUnit;
 
 public class FullBuild extends Build {
 
-	//	@Override
-	//	protected void runUnitTests() {
-	//		super.runUnitTests();
-	//		final File report = new File(testReportDir(), "jacoco.exec");
-	//		final JakeJavaProcess process = JakeJavaProcess.of().andAgent(this.baseDir("build/libs/jacoco-agent/jacocoagent.jar"),
-	//				"destfile="+ report.getAbsolutePath());
-	//		jakeUnit().forkKeepingSameClassPath(process).launchAll(testClassDir());
-	//	}
 
 	@Override
 	protected JakeUnit jakeUnit() {
