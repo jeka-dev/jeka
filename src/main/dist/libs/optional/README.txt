525d416f72b5c27eb584d8a3c3ab3080fee21c9f
i19451 <i19451@WDD06320.resnp.sysnp.shared.fortis>
2014-09-17 17:37:54 +0200

    Jacoco

---
+++ src/main/dist/libs/optional/README.txt
@@ -0,0 +1,2 @@
+Put here libraries that may are likely to be use in some build. 
+libraries located here are not embedded in any class loader unless you do it specifically.
---
+++ src/main/java/org/jake/java/test/jacoco/Jakeoco.java
@@ -0,0 +1,80 @@
+package org.jake.java.test.jacoco;
+
+import java.io.File;
+
+import org.jake.JakeLocator;
+import org.jake.JakeLog;
+import org.jake.java.JakeBuildJava;
+import org.jake.java.JakeJavaProcess;
+import org.jake.java.test.junit.JakeUnit;
+
+public class Jakeoco {
+
+	private final File agent;
+
+	private final boolean enabled;
+
+	private final File destFile;
+
+	private Jakeoco(File agent,boolean enabled, File destFile) {
+		super();
+		this.agent = agent;
+		this.enabled = enabled;
+		this.destFile = destFile;
+	}
+
+	public static Jakeoco of(File destFile) {
+		return new Jakeoco(defaultAgentFile(), true, destFile);
+	}
+
+	public static Jakeoco of(JakeBuildJava jakeBuildJava) {
+		final File agent = jakeBuildJava.baseDir("build/libs/jacoco-agent/jacocoagent.jar");
+		final File agentFile = agent.exists() ? agent : defaultAgentFile();
+		if (!agentFile.exists()) {
+			throw new IllegalStateException("No jacocoagent.jar found neither in " + defaultAgentFile().getAbsolutePath()
+					+ " nor in " + agent.getAbsolutePath() );
+		}
+		return new Jakeoco(agentFile, true, new File(jakeBuildJava.testReportDir(), "/jacoco/jacoco.exec"));
+	}
+
+	public Jakeoco withAgent(File jacocoagent) {
+		return new Jakeoco(jacocoagent, enabled, destFile);
+	}
+
+	public JakeUnit enhance(JakeUnit jakeUnit) {
+		if (jakeUnit.isForked()) {
+			JakeJavaProcess process = jakeUnit.getFork();
+			process = process.andAgent(destFile, options());
+			return jakeUnit.fork(process);
+		}
+		final JakeJavaProcess process = JakeJavaProcess.of().andAgent(agent, options());
+		return jakeUnit.forkKeepingSameClassPath(process).withPostAction(new Reporter());
+	}
+
+	private String options() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("destfile=").append(destFile.getAbsolutePath());
+		builder.append("enabled="+ enabled);
+		return builder.toString();
+	}
+
+	private static File defaultAgentFile() {
+		return new File(JakeLocator.optionalLibsDir(), "jacocoagent.jar");
+	}
+
+	private class Reporter implements Runnable {
+
+		@Override
+		public void run() {
+			if (enabled) {
+				JakeLog.info("Jacoco report created at " + destFile.getAbsolutePath());
+			}
+
+		}
+
+	}
+
+
+
+
+}
