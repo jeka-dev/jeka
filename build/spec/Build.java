d4bbd75138e52150e23a3c5789fb4fe1bb3676cb
djeang <djeangdev@yahoo.fr>
2014-09-28 21:19:08 +0200

    ivy

--- build/spec/Build.java
+++ build/spec/Build.java
@@ -2,7 +2,6 @@
 import java.io.File;
 import java.util.zip.Deflater;
 
-import org.jake.JakeDoc;
 import org.jake.JakeLog;
 import org.jake.JakeOptions;
 import org.jake.java.build.JakeBuildJava;
@@ -11,36 +10,37 @@ import org.jake.utils.JakeUtilsFile;
 
 public class Build extends JakeBuildJava {
 
-
-	// Just to run directly the whole build bypassing the bootstrap mechanism.
-	// (Was needed in first place to build Jake with itself).
+	// Just to run directly the whole build bypassing the Jake bootstrap mechanism.
+	// Was necessary in first place to build Jake with itself.
 	public static void main(String[] args) {
 		JakeOptions.forceVerbose(true);
 		new Build().base();
 	}
 
-	@Override
-	public void base() {
-		super.base();
-		pack();
-	}
-
-	// Include a time stamped version file as resource
+	// Include a time stamped version file as resource.
 	@Override
 	protected void generateResources() {
 		final File versionFile = new File(generatedResourceDir(),"org/jake/version.txt");
 		JakeUtilsFile.writeString(versionFile, version(), false);
 	}
 
+	// Normally the default method just go to compile and unit tests.
+	// Here we tell that the default method should also package the application
+	@Override
+	public void base() {
+		super.base();
+		pack();
+	}
+
+	// Include the making of the distribution into the application packaging.
 	@Override
 	public void pack() {
 		super.pack();
 		distrib();
 	}
 
-	// Create the whole distribution
-	@JakeDoc("Make the whole distribution.")
-	public void distrib() {
+	// Create the whole distribution.
+	private void distrib() {
 		final File distribDir = ouputDir("jake-distrib");
 		final File distripZipFile = ouputDir("jake-distrib.zip");
 
