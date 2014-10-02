fd71eb30c17c86033b48f725e2a5cf7d4121f399
i19451 <i19451@WDD06320.resnp.sysnp.shared.fortis>
2014-10-02 19:37:26 +0200

    ivytest

--- src/main/java/org/jake/ivy/JakeIvy.java
+++ src/main/java/org/jake/ivy/JakeIvy.java
@@ -4,25 +4,34 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.ivy.Ivy;
+import org.apache.ivy.core.module.descriptor.Artifact;
 import org.apache.ivy.core.module.descriptor.DefaultDependencyDescriptor;
 import org.apache.ivy.core.module.descriptor.DefaultModuleDescriptor;
 import org.apache.ivy.core.module.id.ModuleRevisionId;
 import org.apache.ivy.core.report.ResolveReport;
+import org.apache.ivy.core.resolve.IvyNode;
 import org.apache.ivy.core.resolve.ResolveOptions;
 import org.apache.ivy.core.retrieve.RetrieveOptions;
 import org.apache.ivy.core.settings.IvySettings;
-import org.apache.ivy.plugins.resolver.DependencyResolver;
 import org.apache.ivy.plugins.resolver.IBiblioResolver;
+import org.apache.ivy.util.DefaultMessageLogger;
+import org.apache.ivy.util.Message;
 
 public class JakeIvy {
 
 	public void retrieve() {
-		final DependencyResolver dependencyResolver = new IBiblioResolver();
+		final IBiblioResolver dependencyResolver = new IBiblioResolver();
+		dependencyResolver.setRoot("http://i-net1102e-prod:8081/nexus/content/groups/bnppf-secured");
+		dependencyResolver.setM2compatible(true);
+		dependencyResolver.setUseMavenMetadata(true);
+		dependencyResolver.setName("nexus"); // Name is necessary to avoid NPE
 
 		final IvySettings ivySettings = new IvySettings();
 		ivySettings.addResolver(dependencyResolver);
+		ivySettings.setDefaultResolver("nexus");  // Setting a default resolver is necessary
 
 		final Ivy ivy = Ivy.newInstance(ivySettings);
+		ivy.getLoggerEngine().setDefaultLogger(new DefaultMessageLogger(Message.MSG_DEBUG));
 
 		final ModuleRevisionId thisModuleRevisionId = ModuleRevisionId.newInstance(
 				"mygroupId",
@@ -30,22 +39,31 @@ public class JakeIvy {
 				"myversion"
 				);
 
-		final ModuleRevisionId dependee = ModuleRevisionId.newInstance("org.apache.tomcat", "tomcat-catalina", "8.0.12");
+		final ModuleRevisionId dependee = ModuleRevisionId.newInstance("org.springframework", "spring-jdbc", "3.0.0.RELEASE");
+		//final ModuleRevisionId dependee = ModuleRevisionId.newInstance("org.hibernate", "hibernate-core", "3.6.10.Final");
 
 		// 1st create an ivy module (this always(!) has a "default" configuration already)
 		final DefaultModuleDescriptor moduleDescriptor = DefaultModuleDescriptor.newDefaultInstance(thisModuleRevisionId);
 
 		// don't go transitive here, if you want the single artifact
-		final DefaultDependencyDescriptor dependencyDescriptor = new DefaultDependencyDescriptor(moduleDescriptor, dependee, false, false, false);
+		final boolean transitive = true;
+		final DefaultDependencyDescriptor dependencyDescriptor = new DefaultDependencyDescriptor(moduleDescriptor, dependee, false, false, transitive);
+
 
 		// map to master to just get the code jar. See generated ivy module xmls from maven repo
 		// on how configurations are mapped into ivy. Or check
 		// e.g. http://lightguard-jp.blogspot.de/2009/04/ivy-configurations-when-pulling-from.html
-		dependencyDescriptor.addDependencyConfiguration("default", "master");
+		// dependencyDescriptor.addDependencyConfiguration("default", "master"); 
+		
+		// To get more than 1 artifact i need to declare "compile" and not "master"
+		dependencyDescriptor.addDependencyConfiguration("default", "compile");
+		
+		
 		moduleDescriptor.addDependency(dependencyDescriptor);
 
 		// now resolve
 		final ResolveOptions resolveOptions = new ResolveOptions().setConfs(new String[]{"default"});
+		resolveOptions.setTransitive(transitive);
 		ResolveReport reportResolver;
 		try {
 			reportResolver = ivy.resolve(moduleDescriptor, resolveOptions);
@@ -58,6 +76,16 @@ public class JakeIvy {
 
 			throw new RuntimeException(reportResolver.getAllProblemMessages().toString());
 		}
+		for (final Object node : reportResolver.getDependencies()) {
+			final IvyNode ivyNode = (IvyNode) node;
+			final Artifact[] artifacts = ivyNode.getAllArtifacts();
+			for (final Artifact artifact : artifacts) {
+				System.out.println("*********************************"+ artifact);
+
+			}
+
+		}
+
 
 		final String filePattern = new File("out").getAbsolutePath() +"/[artifact](-[classifier]).[ext]";
 		final RetrieveOptions retrieveOptions = new RetrieveOptions().setConfs(new String[]{"default"});
---
+++ src/test/java/org/jake/ivy/JakeIvyRunner.java
@@ -0,0 +1,9 @@
+package org.jake.ivy;
+
+public class JakeIvyRunner {
+
+	public static void main(String[] args) {
+		new JakeIvy().retrieve();
+	}
+
+}
