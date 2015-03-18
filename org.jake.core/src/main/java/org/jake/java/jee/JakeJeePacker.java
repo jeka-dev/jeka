113ed4f943ce33c4e80bd97c126a01d38ca9b645
djeang <djeangdev@yahoo.fr>
2015-03-18 20:21:32 +0100

    jee

--- org.jake.core/src/main/java/org/jake/java/jee/JakeJeePacker.java
+++ org.jake.core/src/main/java/org/jake/java/jee/JakeJeePacker.java
@@ -3,6 +3,7 @@ package org.jake.java.jee;
 import java.io.File;
 
 import org.jake.JakeDir;
+import org.jake.JakeException;
 import org.jake.java.build.JakeJavaBuild;
 
 /**
@@ -21,14 +22,18 @@ public class JakeJeePacker {
 	}
 
 	public void packWar(File webappSrc, File warDirDest, File warFileDest) {
+		if (! new File(webappSrc, "WEB-INF/web.xml").exists()) {
+			throw new JakeException("the directory " + webappSrc.getPath()
+					+ " does not contains WEB-INF" + File.separator + "web.xml file");
+		}
 		final JakeDir dir = JakeDir.of(warDirDest).copyInDirContent(webappSrc)
 				.sub("WEB-INF/classes").copyInDirContent(build.classDir())
-				.sub("../libs").copyInFiles(build.depsFor(JakeJavaBuild.RUNTIME));
+				.sub("../lib").copyInFiles(build.depsFor(JakeJavaBuild.RUNTIME));
 		dir.zip().to(warFileDest);
 	}
 
-	public void packEar(Iterable<File> warFiles, File earSrc, File dest) {
-
+	public void packEar(Iterable<File> warFiles, File earSrc, File destDir, File destFile) {
+		JakeDir.of(destDir).copyInDirContent(earSrc).copyInFiles(warFiles).zip().to(destFile);
 	}
 
 
