1df3418bd3b20ebe0362478b2e2d584679fe2fd4
djeang <djeangdev@yahoo.fr>
2015-04-13 17:12:32 +0200

    renaming

--- org.jerkar.core/src/test/java/org/jerkar/java/JkClasspathTest.java
+++ org.jerkar.core/src/test/java/org/jerkar/java/JkClasspathTest.java
@@ -15,8 +15,8 @@ public class JkClasspathTest {
 		final URL sampleJarUrl = JkClasspathTest.class.getResource("jarWithTwoClassesIn.jar");
 		final File sampleJar = new File(sampleJarUrl.toURI().getPath());
 		final JkClasspath classpath = JkClasspath.of(sampleJar);
-		assertEquals(sampleJar, classpath.getEntryContainingClass("org.jerkar.JkBuildBase"));
-		assertEquals(sampleJar, classpath.getEntryContainingClass("org.jerkar.JkBuildBase$ActionDescription"));
+		assertEquals(sampleJar, classpath.getEntryContainingClass("org.jake.JakeBuildBase"));
+		assertEquals(sampleJar, classpath.getEntryContainingClass("org.jake.JakeBuildBase$ActionDescription"));
 		assertEquals(null, classpath.getEntryContainingClass("no.existing.MyClass"));
 	}
 
