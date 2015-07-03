b189ccf1587aee6621f15cada12d1ee0f62480f0
djeang <djeangdev@yahoo.fr>
2015-07-03 23:30:48 +0200

    doc

--- org.jerkar.core/src/main/java/org/jerkar/api/package-info.java
+++ org.jerkar.core/src/main/java/org/jerkar/api/package-info.java
@@ -3,7 +3,6 @@
  * Core Jerkar API are a set of classes that can be reused/embedded in any java program
  * and have no dependency on the Jerkar automation tool.
  * 
- * For now this API has been design to run in batch program (as build or automated script are)
- * and focus on providing friendly concise syntax rather on optimized processing.
+ * You can use it for describing your build files or embbed it in any of your program.
  */
 package org.jerkar.api;
