97f0d9f9a67c393e5f66c36a047930dca0b64b94
djeang <djeangdev@yahoo.fr>
2016-11-15 21:27:33 +0100

    fix publish parameter

--- org.jerkar.core/build/def/org/jerkar/AbstractBuild.java
+++ org.jerkar.core/build/def/org/jerkar/AbstractBuild.java
@@ -42,8 +42,8 @@ public abstract class AbstractBuild extends JkJavaBuild {
         if (JkOptions.containsKey("jkPublisherUrl")) {
             return JkPublishRepos.maven(JkOptions.get("jkPublisherUrl"));
         }
-        return JkPublishRepos.ossrh(JkOptions.get("ossrh.username"),
-                JkOptions.get("ossrh.password"), pgp()).withUniqueSnapshot(true);
+        return JkPublishRepos.ossrh(JkOptions.get("repo.ossrh.username"),
+                JkOptions.get("repo.ossrh.password"), pgp()).withUniqueSnapshot(true);
     }
 
 }
