9bad0e3e4f4aa406af16d897011bc53e18673c6c
djeang <djeangdev@yahoo.fr>
2016-11-15 21:58:10 +0100

    comment

--- org.jerkar.core/build/def/org/jerkar/RunDoPublish.java
+++ org.jerkar.core/build/def/org/jerkar/RunDoPublish.java
@@ -4,6 +4,12 @@ import org.jerkar.tool.JkInit;
 
 class RunDoPublish {
 
+    /**
+     * To publish run this method then go to https://oss.sonatype.org and log with 'djeang' user name.
+     * Go to 'Staging repositories' then go down to get the the org.jerkar repository and close it.
+     * Wait a bit then clicking on this repository (marked as closed), click on 'release' at the top bar.
+     * Wait some hours/days before see it on Maven central !
+     */
     public static void main(String[] args) {
         JkInit.instanceOf(CoreBuild.class, args).doPublish();
     }
