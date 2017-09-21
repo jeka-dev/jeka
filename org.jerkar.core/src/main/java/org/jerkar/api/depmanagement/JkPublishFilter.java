ac42ada220ceabc4ab972b6fcb377fcc83bec81b
djeang <djeangdev@yahoo.fr>
2017-09-21 12:19:02 +0200

    closes #95

--- org.jerkar.core/src/main/java/org/jerkar/api/depmanagement/JkPublishFilter.java
+++ org.jerkar.core/src/main/java/org/jerkar/api/depmanagement/JkPublishFilter.java
@@ -24,10 +24,12 @@ public interface JkPublishFilter extends Serializable {
     /**
      * A filter accepting only snapshot versioned module.
      */
-    JkPublishFilter ACCEPT_SNAPSHOT_ONLY = (JkPublishFilter) versionedModule -> versionedModule.version().isSnapshot();
+    JkPublishFilter ACCEPT_SNAPSHOT_ONLY =
+            (JkPublishFilter) versionedModule -> versionedModule.version().isSnapshot();
 
     /**
      * A filter accepting only non-snapshot versioned module.
      */
-    JkPublishFilter ACCEPT_RELEASE_ONLY = (JkPublishFilter) versionedModule -> !versionedModule.version().isSnapshot();
+    JkPublishFilter ACCEPT_RELEASE_ONLY =
+            (JkPublishFilter) versionedModule -> !versionedModule.version().isSnapshot();
 }
