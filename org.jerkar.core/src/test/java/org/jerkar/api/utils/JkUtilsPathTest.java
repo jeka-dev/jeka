644c05fb906d0b7acc1ca1ed2ee594731703ab58
djeang <djeangdev@yahoo.fr>
2017-09-29 01:13:18 +0200

    clean

--- org.jerkar.core/src/test/java/org/jerkar/api/utils/JkUtilsPathTest.java
+++ org.jerkar.core/src/test/java/org/jerkar/api/utils/JkUtilsPathTest.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.junit.Test;
 
@@ -19,13 +20,13 @@ public class JkUtilsPathTest {
                 .getResource("samplefolder/subfolder/sample.txt");
         final Path source = Paths.get(sampleFileUrl.toURI()).getParent().getParent();
         final Path target = Files.createTempDirectory("copydirtest");
-        JkUtilsPath.copyDirContent(source, target);
+        JkUtilsPath.copyDirContent(source, target, StandardCopyOption.REPLACE_EXISTING);
         System.out.println(target);
         assertTrue(Files.exists(target.resolve("subfolder/sample.txt")));
 
         final Path subfolder = Paths.get(sampleFileUrl.toURI()).getParent();
         final Path target2 = Files.createTempDirectory("copydirtest");
-        JkUtilsPath.copyDirContent(subfolder, target2);
+        JkUtilsPath.copyDirContent(subfolder, target2, StandardCopyOption.REPLACE_EXISTING);
         System.out.println(target2);
         assertTrue(Files.exists(target2.resolve("sample.txt")));
 
