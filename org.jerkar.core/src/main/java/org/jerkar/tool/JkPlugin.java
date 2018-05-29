50ad9c6a988133dacafaa896d6c229a8a22bc3c8
djeang <djeangdev@yahoo.fr>
2018-05-29 23:35:59 +0200

    comments

--- org.jerkar.core/src/main/java/org/jerkar/tool/JkPlugin.java
+++ org.jerkar.core/src/main/java/org/jerkar/tool/JkPlugin.java
@@ -15,15 +15,15 @@ package org.jerkar.tool;
  * exemple command line <code>jerkar sonarQube#run</code> will invoke the <code>run(JkBuild)</code> method of plugin class JkBuildPluginSonarQube.<br/>
  * For <code>apply</code> method, one can use <code>jerkar myPlugin#</code> short-hand instead of <code>jerkar myPlugin#apply</code>.
  * <p>
- * All plugin class must be named as <code>JkPluginXxxxxx</code> where xxxxxx stands for the plugin name. This
- * is necessary in order to find plugin class from its name accept the classpath.
+ * All plugin classes must be named as <code>JkPluginXxxxxx</code> where xxxxxx stands for the plugin name. This
+ * is necessary in order to find plugin classes from its name in the classpath.
  * <p>
  * A plugin can be configured at runtime by injecting data in its instance fields by passing argument <i>pluginName#</i>
  * followed by the field name. For example <code>jerkar eclise#generatefiles -eclipse#useVarPath=true</code>.
  *
  * <h5>Configuration</h5>
- * One may need to configure a plugin in the build class itself accept order to no mention the configuration of the
- * command line each time it is invoked. For such build class writer has to instantiate and configure programmatically
+ * One may need to configure a plugin in the build class itself in order to avoiding to mention the configuration in the
+ * command line each time it is invoked. For such build classses, writer has to instantiate and configure programmatically
  * in the build class then register it by invoking JkBuild#register(JkPlugin) method.
  *
  * <h5>Documentation</h5>
