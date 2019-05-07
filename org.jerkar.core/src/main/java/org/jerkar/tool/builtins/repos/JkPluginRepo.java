57eb714243b56828ece18b37488d2411b36f04c7
djeang <djeangdev@yahoo.fr>
2019-05-07 09:18:11 +0200

    Clean repoPlugin

--- org.jerkar.core/src/main/java/org/jerkar/tool/builtins/repos/JkPluginRepo.java
+++ org.jerkar.core/src/main/java/org/jerkar/tool/builtins/repos/JkPluginRepo.java
@@ -17,9 +17,9 @@ import java.util.Map;
         "  To select a 'download' (respectively a 'publish') repository, this plugin check in order : \n" +
         "    - the 'repos#downloadRepoName' option which designate the name of the configured plugin\n" +
         "    - the 'repos#downloadUrl', 'repos#downloadUsername' and 'repos#downloadPassword' options to instantiate a repo based on these values\n" +
-        "    - the 'Repos.download.url', 'Repos.download.username' and 'Repo.download.password' options to instantiate repo based on these values\n" +
+        "    - the 'Repos.download.url', 'Repos.download.username' and 'Repo.download.password' global options to instantiate repo based on these values\n" +
         "    - the default repo returned by JkRepo#ofMavenCentral() for downloading and local repository for publishing.\n" +
-        "  To postPluginSetup a named repository, add following properties into your [Jerkar_user_home]/options.properties file :\n" +
+        "  To configure a named repository, add following properties into your [Jerkar_user_home]/options.properties file :\n" +
         "    'Repos.[name].url', 'Repos.[value].username' and 'Repos.[of].password'"
 )
 public class JkPluginRepo extends JkPlugin {
@@ -79,25 +79,30 @@ public class JkPluginRepo extends JkPlugin {
     }
 
     @JkDoc("Displays active and configured repositories.")
-    public void display() {
+    public void info() {
         StringBuilder sb = new StringBuilder();
-
         JkRepo download = downloadRepository();
-
         sb.append("Download repository")
                 .append("\n  url : " + download.getUrl());
         if (download.getCredential() != null) {
-            String downloadPwd = JkUtilsString.isBlank(download.getCredential().getPassword()) ? "" : downloadPassword.substring(0, 1) + "*******";
+            String downloadPwd = JkUtilsString.isBlank(download.getCredential().getPassword()) ? ""
+                    : downloadPassword.substring(0, 1) + "*******";
             sb.append("\n  username : " + download.getCredential().getUserName())
                     .append("\n  password : " + downloadPwd);
         }
-
+        JkRepo publish = publishRepository();
+        sb.append("\n").append("Publish repository")
+                .append("\n  url : " + publish.getUrl());
+        if (publish.getCredential() != null) {
+            String publishPwd = JkUtilsString.isBlank(publish.getCredential().getPassword()) ? ""
+                    : publishPassword.substring(0, 1) + "*******";
+            sb.append("\n  username : " + publish.getCredential().getUserName())
+                    .append("\n  password : " + publishPwd);
+        }
         for (Map.Entry<String, String> entry : JkRepoConfigOptionLoader.allRepositoryOptions().entrySet()) {
             sb.append("\n" + entry.getKey() + " : ").append(entry.getValue());
         }
         JkLog.info(sb.toString());
     }
-
-
 
 }
