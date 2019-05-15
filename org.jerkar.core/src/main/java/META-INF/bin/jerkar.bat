60b54f6ded707587c322a18cd518966ac8df72b5
djeang <djeangdev@yahoo.fr>
2019-05-15 21:45:50 +0200

    Polish

--- org.jerkar.core/src/main/java/META-INF/bin/jerkar.bat
+++ org.jerkar.core/src/main/java/META-INF/bin/jerkar.bat
@@ -6,6 +6,7 @@
 @rem set terminal encoding to utf-8
 chcp 65001 > nul
 
+if not "%JERKAR_JDK% == "" set "JAVA_HOME=%JERKAR_JDK%"
 if "%JAVA_HOME%" == "" set "JAVA_CMD=java"
 if not "%JAVA_HOME%" == "" set "JAVA_CMD=%JAVA_HOME%\bin\java"
 
