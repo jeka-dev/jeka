4ecd8c9dc11e06ad31e80cda8d9f09df78ff0e53
djeang <djeangdev@yahoo.fr>
2019-05-21 21:38:10 +0200

    Fix

--- org.jerkar.core/jerkar/def/org/jerkar/CoreBuild.java
+++ org.jerkar.core/jerkar/def/org/jerkar/CoreBuild.java
@@ -26,7 +26,7 @@ public class CoreBuild extends JkRun {
 
     public static final JkArtifactId DISTRIB_FILE_ID = JkArtifactId.of("distrib", "zip");
 
-    private static final String VERSION = "0.7.0-SNAPSHOT";
+    private static final String VERSION = "0.7.0.RC3";
 
     final JkPluginJava javaPlugin = getPlugin(JkPluginJava.class);
 
