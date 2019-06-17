08b0c9d1678947440f1650a93c18b671f4eaebc0
djeang <djeangdev@yahoo.fr>
2019-06-17 17:38:57 +0200

    #  Introduce ClassGraph

--- dev.jeka.core/src/main/java/dev/jeka/core/api/java/JkInternalClasspathScanner.java
+++ dev.jeka.core/src/main/java/dev/jeka/core/api/java/JkInternalClasspathScanner.java
@@ -1,11 +1,26 @@
 package dev.jeka.core.api.java;
 
-import java.util.Set;
-import java.util.function.Predicate;
+import dev.jeka.core.api.utils.JkUtilsReflect;
 
+import java.util.Set;
+
+/**
+ * Not part of public api
+ */
 public interface JkInternalClasspathScanner {
 
+    static final JkInternalClasspathScanner INSTANCE = of();
+
     Set<Class<?>> loadClassesMatching(ClassLoader classLoader, String ... globPatterns);
+
+    static JkInternalClasspathScanner of() {
+        String IMPL_CLASS = "dev.jeka.core.api.java.embedded.classgraph.ClassGraphClasspathScanner";
+        Class<JkInternalClasspathScanner> clazz = JkClassLoader.ofCurrent().loadIfExist(IMPL_CLASS);
+        if (clazz != null) {
+            return JkUtilsReflect.invokeStaticMethod(clazz, "of");
+        }
+        return JkInternalEmbeddedClassloader.createCrossClassloaderProxy(JkInternalClasspathScanner.class, IMPL_CLASS, "of");
+    }
 
 
 }
--- dev.jeka.core/src/main/java/dev/jeka/core/api/java/embedded/classgraph/ClassGraphClasspathScanner.java
+++ dev.jeka.core/src/main/java/dev/jeka/core/api/java/embedded/classgraph/ClassGraphClasspathScanner.java
@@ -2,21 +2,22 @@ package dev.jeka.core.api.java.embedded.classgraph;
 
 import dev.jeka.core.api.java.JkInternalClasspathScanner;
 import io.github.classgraph.ClassGraph;
-import io.github.classgraph.ClassInfo;
-import io.github.classgraph.ScanResult;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Predicate;
 
 class ClassGraphClasspathScanner implements JkInternalClasspathScanner {
+
+    static ClassGraphClasspathScanner of() {
+        return new ClassGraphClasspathScanner();
+    }
 
     @Override
     public Set<Class<?>> loadClassesMatching(ClassLoader classLoader, String... globPatterns) {
         ClassGraph classGraph = new ClassGraph()
                 .overrideClassLoaders(classLoader)
                 .whitelistPaths(globPatterns);
-        return new HashSet<Class<?>>(classGraph.scan().getAllClasses().loadClasses());
+        return new HashSet<>(classGraph.scan().getAllClasses().loadClasses());
     }
 
 }
