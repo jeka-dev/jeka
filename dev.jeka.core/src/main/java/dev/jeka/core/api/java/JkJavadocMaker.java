3816f187a7f95fab8a6e5890e5de8d03e9ecc589
djeang <djeangdev@yahoo.fr>
2019-06-27 23:26:15 +0200

    clean javadoc

--- dev.jeka.core/src/main/java/dev/jeka/core/api/java/JkJavadocMaker.java
+++ dev.jeka.core/src/main/java/dev/jeka/core/api/java/JkJavadocMaker.java
@@ -4,22 +4,18 @@ package dev.jeka.core.api.java;
 import dev.jeka.core.api.file.JkPathSequence;
 import dev.jeka.core.api.file.JkPathTree;
 import dev.jeka.core.api.file.JkPathTreeSet;
-import dev.jeka.core.api.system.JkException;
 import dev.jeka.core.api.system.JkLog;
-import dev.jeka.core.api.system.JkProcess;
-import dev.jeka.core.api.utils.*;
+import dev.jeka.core.api.utils.JkUtilsPath;
 
-import javax.tools.*;
+import javax.tools.DocumentationTool;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 import java.io.*;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Offers fluent interface for producing Javadoc.
@@ -34,19 +30,16 @@ public final class JkJavadocMaker {
 
     private final List<String> extraArgs;
 
-    private final Class<?> doclet;
-
     private final Iterable<Path> classpath;
 
     private final Path outputDir;
 
     private final Path zipFile;
 
-    private JkJavadocMaker(JkPathTreeSet srcDirs, Class<?> doclet, Iterable<Path> classpath,
+    private JkJavadocMaker(JkPathTreeSet srcDirs, Iterable<Path> classpath,
                            List<String> extraArgs, Path outputDir, Path zipFile) {
         this.srcDirs = srcDirs;
         this.extraArgs = extraArgs;
-        this.doclet = doclet;
         this.classpath = classpath;
         this.outputDir = outputDir;
         this.zipFile = zipFile;
@@ -57,7 +50,7 @@ public final class JkJavadocMaker {
      * the specified directory then compacted in the specified zip file.
      */
     public static JkJavadocMaker of(JkPathTreeSet sources, Path outputDir, Path zipFile) {
-        return new JkJavadocMaker(sources, null, null, new LinkedList<>(), outputDir, zipFile);
+        return new JkJavadocMaker(sources, null,  new LinkedList<>(), outputDir, zipFile);
     }
 
     /**
@@ -65,25 +58,7 @@ public final class JkJavadocMaker {
      * the specified directory.
      */
     public static JkJavadocMaker of(JkPathTreeSet sources, Path outputDir) {
-        return new JkJavadocMaker(sources, null, null, new LinkedList<>(), outputDir, null);
-    }
-
-
-    /**
-     * Returns the zip file containing all the produced Javadoc.
-     */
-    public Path zipFile() {
-        return zipFile;
-    }
-
-    /**
-     * Returns a {@link JkJavadocMaker} identical to this one but using the specified doclet.
-     */
-    public JkJavadocMaker withDoclet(Class<?> doclet) {
-        if (doclet == null) {
-            return this;
-        }
-        return new JkJavadocMaker(srcDirs, doclet, classpath, extraArgs, outputDir, zipFile);
+        return new JkJavadocMaker(sources, null,  new LinkedList<>(), outputDir, null);
     }
 
     /**
@@ -99,136 +74,63 @@ public final class JkJavadocMaker {
     public JkJavadocMaker andOptions(List<String> options) {
         final List<String> list = new LinkedList<>(this.extraArgs);
         list.addAll(options);
-        return new JkJavadocMaker(srcDirs, doclet, classpath, list, outputDir, zipFile);
+        return new JkJavadocMaker(srcDirs, classpath, list, outputDir, zipFile);
     }
 
     /**
      * Returns a {@link JkJavadocMaker} identical to this one but using the specified classpath.
      */
     public JkJavadocMaker withClasspath(Iterable<Path> classpath) {
-        return new JkJavadocMaker(srcDirs, doclet, JkUtilsPath.disambiguate(classpath), extraArgs, outputDir, zipFile);
+        return new JkJavadocMaker(srcDirs, JkUtilsPath.disambiguate(classpath), extraArgs, outputDir, zipFile);
     }
 
     /**
      * Actually processes and creates the javadoc files.
      */
     public void process() {
-        Runnable task = () -> {
-            if (this.srcDirs.hasNoExistingRoot()) {
-                JkLog.warn("No sources found in " + this.srcDirs);
-                return;
-            }
-            if (JkUtilsJdk.runningMajorVersion() >= 11) {
-                executeCmdLine(toArguments(outputDir, false));
-                //executeTool(outputDir, srcDirs.getFiles());
-            } else {
-                final List<String> args = toArguments(outputDir, true);
-                OutputStream info = JkLog.getOutputStream();
-                OutputStream warn = JkUtilsIO.nopPrintStream();   // error and log produces very verbose logs in jdoclet
-                OutputStream error = JkUtilsIO.nopPrintStream();
-                if (JkLog.verbosity() == JkLog.Verbosity.VERBOSE) {
-                    warn = JkLog.getOutputStream();
-                    error = JkLog.getErrorStream();
-                }
-                JkUtilsPath.createDirectories(outputDir);
-                execute(doclet, info, warn, error, args);
-            }
-            if (Files.exists(outputDir) && zipFile != null) {
-                JkPathTree.of(outputDir).zipTo(zipFile);
-            }
-        };
-        JkLog.execute("Generating javadoc", task);
-    }
-
-    private List<String> toArguments(Path outputDir, boolean withDocletPath) {
-        final List<String> list = new LinkedList<>();
-        list.add("-sourcepath");
-        list.add(JkPathSequence.of(this.srcDirs.getRootDirsOrZipFiles()).toString());
-        list.add("-d");
-        list.add(outputDir.toAbsolutePath().toString());
-        if (JkLog.verbosity() == JkLog.Verbosity.VERBOSE) {
-            list.add("-verbose");
-        } else {
-            list.add("-quiet");
+        JkLog.startTask("Generating javadoc");
+        if (this.srcDirs.hasNoExistingRoot()) {
+            JkLog.warn("No sources found in " + this.srcDirs);
+            return;
         }
-        if (classpath != null && classpath.iterator().hasNext()) {
-            list.add("-classpath");
-            list.add(JkPathSequence.of(this.classpath).toString());
+        executeTool(outputDir, srcDirs, classpath);
+        if (Files.exists(outputDir) && zipFile != null) {
+            JkPathTree.of(outputDir).zipTo(zipFile);
         }
-        if (withDocletPath) {
-            list.add("-docletpath");
-            list.add(JkUtilsJdk.toolsJar().toString());
-        }
-        list.addAll(extraArgs);
-
-        for (final Path sourceFile : this.srcDirs.getFiles()) {
-            if (sourceFile.getFileName().toString().endsWith(".java")) {
-                list.add(sourceFile.toString());
-            }
-
-        }
-        return list;
-    }
-
-    private static void execute(Class<?> doclet, OutputStream normalStream, OutputStream warnStream,
-                                OutputStream errorStream, List<String> args) {
-
-        final String docletString = doclet != null ? doclet.getName()
-                : "com.sun.tools.doclets.standard.Standard";
-        final Class<?> mainClass = getJavadocMainClass();
-        JkUtilsReflect.newInstance(mainClass);
-        final Method method = JkUtilsReflect.getMethod(mainClass, "execute", String.class,
-                PrintWriter.class, PrintWriter.class, PrintWriter.class, String.class,
-                String[].class);
-        JkUtilsReflect.invoke(null, method, "Javadoc", new PrintWriter(errorStream),
-                new PrintWriter(warnStream), new PrintWriter(normalStream), docletString, args.toArray(new String[0]));
-    }
-
-    private static Class<?> getJavadocMainClass() {
-        final JkClassLoader classLoader = JkClassLoader.ofCurrent();
-        Class<?> mainClass = classLoader.loadIfExist(JAVADOC_MAIN_CLASS_NAME);
-        if (mainClass == null) {
-            URL url = JkUtilsPath.toUrl(JkUtilsJdk.toolsJar());
-            URLClassLoader urlClassLoader = new URLClassLoader(new URL[] {url});
-            mainClass = JkClassLoader.of(urlClassLoader).loadIfExist(JAVADOC_MAIN_CLASS_NAME);
-            if (mainClass == null) {
-                throw new RuntimeException(
-                        "It seems that you are running a JRE instead of a JDK, please run Jeka using a JDK.");
-            }
-        }
-        return mainClass;
-    }
-
-    private static void executeCmdLine(List<String> args) {
-        JkProcess.of("javadoc", args.toArray(new String[0])).withWorkingDir(JkUtilsJdk.javaHome()
-                .resolve("bin")).withFailOnError(true).withLogCommand(true).runSync();
+        JkLog.endTask();
     }
 
     // https://www.programcreek.com/java-api-examples/index.php?api=javax.tools.DocumentationTool
-    private static void executeTool(Path outputDir, List<Path> files) {
+    private static void executeTool(Path outputDir, JkPathTreeSet sourceTreeSet, Iterable<Path> classpath) {
         DocumentationTool tool = ToolProvider.getSystemDocumentationTool();
         try (StandardJavaFileManager fm = tool.getStandardFileManager(null, null, null)) {
             Files.createDirectories(outputDir);
-            Iterable<JavaSourceObject> fileObjects = files.stream()
-                    .filter(path -> path.getFileName().endsWith(".java"))
-                    .map(path -> new JavaSourceObject(path)).collect(Collectors.toList());
-            fm.setLocation(DocumentationTool.Location.DOCUMENTATION_OUTPUT, JkUtilsIterable.listOf(outputDir.toFile()));
+            ///fm.setLocation(DocumentationTool.Location.DOCUMENTATION_OUTPUT, JkUtilsIterable.listOf(outputDir.toFile()));
             Writer writer = new PrintWriter(new OutputStreamWriter(JkLog.getOutputStream(), "UTF-8"));
-            DocumentationTool.DocumentationTask task = tool.getTask(writer, fm, null, null,
-                    null, fileObjects);
-            boolean success = task.call();
-            if (!success) {
-                throw new JkException("Javadoc task failed");
+            List<String> options = new LinkedList<>();
+            options.add("-sourcepath");
+            options.add(JkPathSequence.of(sourceTreeSet.getRootDirsOrZipFiles()).toString());
+            options.add("-subpackages");
+            options.add(".");
+            options.add("-d");
+            options.add(outputDir.toString());
+            if (JkLog.verbosity() == JkLog.Verbosity.VERBOSE) {
+                options.add("-verbose");
+            } else {
+                ///options.add("-quiet");
             }
+            if (classpath != null && classpath.iterator().hasNext()) {
+                options.add("-classpath");
+                options.add(JkPathSequence.of(classpath).toString());
+            }
+            DocumentationTool.DocumentationTask task = tool.getTask(writer, null, null, null,
+                    options, null);
+            options.add("-verbose");
+            ///options.add("*");
+            task.call();
+
         } catch (IOException e) {
             throw new UncheckedIOException(e);
-        }
-    }
-
-    private static class JavaSourceObject extends SimpleJavaFileObject {
-
-        protected JavaSourceObject(Path path) {
-            super(path.toUri(), Kind.SOURCE);
         }
     }
 
