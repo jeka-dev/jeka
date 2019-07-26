16a190b45022d948bc376b0ace162d898163a779
Chocohead <Chocohead@Aroma1997.org>
2019-07-27 00:55:31 +0100

    Fix Windows run scripts
    Fixes #125
    Avoids jeka.bat needing JEKA_HOME to be set before running the script
    Fixes jekaw.bat breaking when in a directory with a space in the path

--- dev.jeka.core/src/main/java/META-INF/bin/jeka.bat
+++ dev.jeka.core/src/main/java/META-INF/bin/jeka.bat
@@ -11,6 +11,8 @@ if "%JAVA_HOME%" == "" set "JAVA_CMD=java"
 if not "%JAVA_HOME%" == "" set "JAVA_CMD=%JAVA_HOME%\bin\java"
 
 if exist %cd%\jeka\boot set "LOCAL_BUILD_DIR=.\jeka\boot\*;"
+if "%JEKA_HOME%" == "" set "JEKA_HOME=%~dp0"
+
 set "COMMAND="%JAVA_CMD%" %JEKA_OPTS% -cp "%LOCAL_BUILD_DIR%%JEKA_HOME%\dev.jeka.jeka-core.jar" dev.jeka.core.tool.Main %*"
 if not "%JEKA_ECHO_CMD%" == "" (
 	@echo on
