90587a8d88a07d150cfb7de1d7442a50899f1047
angibaudj <angibaudj>
2019-10-08 09:51:00 +0200

    Closes #141

--- dev.jeka.core/src/main/java/dev/jeka/core/tool/JkCompileOption.java
+++ dev.jeka.core/src/main/java/dev/jeka/core/tool/JkCompileOption.java
@@ -7,6 +7,12 @@ import java.lang.annotation.*;
 /**
  * Specifies the options of the Jeka compiler for def classes.
  * Normally, one option by annotation is expected tough this annotation is repeatable.
+ * You can declare it has follow :
+ * <pre><code>
+ * @JkCompileOption("-deprecation")
+ * @JkCompileOption({"-processorPath", "/foo/bar"})
+ * </code></pre>
+ *
  *
  * @author Jerome Angibaud
  */
@@ -19,7 +25,7 @@ public @interface JkCompileOption {
      * The dependency to import. It can be a module dependency (as "com.google.guava:guava:18.0")
      * or a file dependency (as "../lib-folder.mylib.jar").
      */
-    String value();
+    String[] value();
 
 
     /**
