c7b6ccc861aab680ffffbdf256b7862d9c992ce1
djeang <djeangdev@yahoo.fr>
2020-04-09 18:54:39 +0200

    clean test

--- dev.jeka.core/src/test/java/dev/jeka/core/api/crypto/gpg/embedded/bc/BcGpgDoerTest.java
+++ dev.jeka.core/src/test/java/dev/jeka/core/api/crypto/gpg/embedded/bc/BcGpgDoerTest.java
@@ -1,25 +1,23 @@
 package dev.jeka.core.api.crypto.gpg.embedded.bc;
 
 import dev.jeka.core.api.crypto.gpg.JkGpgTest;
-import dev.jeka.core.api.file.JkPathFile;
 import dev.jeka.core.api.utils.JkUtilsIO;
-import dev.jeka.core.tool.JkConstants;
 import org.junit.Test;
 
 import java.io.InputStream;
-import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SuppressWarnings("javadoc")
 public class BcGpgDoerTest {
 
     @Test
     public void testSignAndVerify() throws Exception {
-        final JkPathFile path = JkPathFile.of(Paths.get(JkConstants.OUTPUT_PATH +
-                "/test-out/signature.asm")).createIfNotExist();
-        BcGpgDoer.sign(sample(), JkGpgTest.class.getResourceAsStream("secring.gpg"), "",
-                JkUtilsIO.outputStream(path.get().toFile(), false), "jerkar".toCharArray(), true);
-
-        final boolean result = BcGpgDoer.verify(sample(), JkUtilsIO.inputStream(path.get().toFile()),
+        Path asmSignature = Files.createTempFile("jeka", ".asm");
+        InputStream secring = JkGpgTest.class.getResourceAsStream("secring.gpg");
+        BcGpgDoer.sign(sample(), secring, "",
+                JkUtilsIO.outputStream(asmSignature.toFile(), false), "jerkar".toCharArray(), true);
+        final boolean result = BcGpgDoer.verify(sample(), JkUtilsIO.inputStream(asmSignature.toFile()),
                 JkGpgTest.class.getResourceAsStream("pubring.gpg"));
         System.out.println(result);
 
