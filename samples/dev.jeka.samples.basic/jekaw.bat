b65c4f63f8f9c5207b359ac97b2b16f62ec9ee00
M999JAD <Jerome.Angibaud@m-team.be>
2021-11-03 12:41:59 +0100

    Add Kotlin support #159 create mono repo structure

--- samples/dev.jeka.samples.basic/jekaw.bat
+++ samples/dev.jeka.samples.basic/jekaw.bat
@@ -10,8 +10,7 @@ if not "%JEKA_JDK%" == "" set "JAVA_HOME=%JEKA_JDK%"
 if "%JAVA_HOME%" == "" set "JAVA_CMD=java"
 if not "%JAVA_HOME%" == "" set "JAVA_CMD=%JAVA_HOME%\bin\java"
 
-if exist %cd%\jeka\boot set "LOCAL_BUILD_DIR=.\jeka\boot\*;"
-set "COMMAND="%JAVA_CMD%" %JEKA_OPTS% -cp "%LOCAL_BUILD_DIR%%~dp0jeka\wrapper\*" dev.jeka.core.wrapper.Booter %~dp0 %*"
+set "COMMAND="%JAVA_CMD%" %JEKA_OPTS% -cp "%~dp0jeka\wrapper\*" dev.jeka.core.wrapper.Booter "%~dp0." %*"
 if not "%JEKA_ECHO_CMD%" == "" (
 	@echo on
 	echo %COMMAND%
