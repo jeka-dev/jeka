33e365d6f013d679b79420898639177b23d84a9e
djeang <djeangdev@yahoo.fr>
2023-12-03 12:14:42 +0100

    Base def-files compilation cache on MD-5

--- dev.jeka.core/src/main/java/dev/jeka/core/tool/EngineCompilationUpdateTracker.java
+++ dev.jeka.core/src/main/java/dev/jeka/core/tool/EngineCompilationUpdateTracker.java
@@ -13,7 +13,7 @@ import java.util.stream.Stream;
 
 class EngineCompilationUpdateTracker {
 
-    private final static String LAST_UPDATE_FILE_NAME = "def-last-update-time.txt";
+    private final static String LAST_UPDATE_FILE_NAME = "def-hash.txt";
 
     private final Path projectBaseDir;
 
@@ -30,16 +30,14 @@ class EngineCompilationUpdateTracker {
             return true;
         }
         boolean result = isWorkOutdated();
-        JkLog.trace("Cached compile outdated : %s", result);
+        JkLog.trace("Cached compilation outdated : %s", result);
         globallyOutdated = result;
         return result;
     }
 
     void updateCompileFlag() {
-        long defLastUptateTime = lastModifiedAccordingFileAttributes();
-        int fileCount = fileCount();
-        JkLog.trace("Cached file count for compilation : %s", fileCount);
-        writeLastUpdateFile(fileCount, defLastUptateTime, JkJavaVersion.ofCurrent());
+        String hash = hashString();
+        flagFile().deleteIfExist().createIfNotExist().write(hash.getBytes(StandardCharsets.UTF_8));
     }
 
     void deleteCompileFlag() {
@@ -47,12 +45,12 @@ class EngineCompilationUpdateTracker {
     }
 
     private boolean isWorkOutdated() {
-        CountTimestampAndJavaVersion flag = stateFromFlagFile();
-        JkLog.traceStartTask("Checking file last upadte time");
-        CountTimestampAndJavaVersion current = stateFromFlagCurrent();
-        JkLog.traceEndTask();
-        JkLog.trace("Comparing flagged %s and  current %s", flag, current);
-        return !flag.equals(current);
+        if (!flagFile().exists()) {
+            return true;
+        }
+        String currentHash = hashString();
+        String flaggedHash = flagFile().readAsString();
+        return !flaggedHash.equals(currentHash);
     }
 
     private long lastModifiedAccordingFileAttributes() {
@@ -66,58 +64,17 @@ class EngineCompilationUpdateTracker {
                 .reduce(0L, Math::max);
     }
 
-    private void writeLastUpdateFile(int fileCount, long lastModifiedAccordingFileAttributes, JkJavaVersion javaVersion) {
-        Path work = projectBaseDir.resolve(JkConstants.WORK_PATH);
-        if (!Files.exists(work)) {
-            return;
-        }
-        String infoString = String.format("%s;%s;%s", fileCount, lastModifiedAccordingFileAttributes, javaVersion);
-        flagFile()
-                .deleteIfExist()
-                .createIfNotExist()
-                .write(infoString.getBytes(StandardCharsets.UTF_8));
+
+
+    private String hashString() {
+        String md5 = JkPathTree.of(projectBaseDir.resolve(JkConstants.DEF_DIR)).checksum("md5");
+        return md5 + ":" + JkJavaVersion.ofCurrent();
     }
 
     private JkPathFile flagFile() {
         return JkPathFile.of(projectBaseDir.resolve(JkConstants.WORK_PATH).resolve(LAST_UPDATE_FILE_NAME));
     }
 
-    private CountTimestampAndJavaVersion stateFromFlagCurrent() {
-        long defLastUptateTime = lastModifiedAccordingFileAttributes();
-        JkLog.traceStartTask("counting files");
-        int count = fileCount();
-        JkLog.traceEndTask();
-        return new CountTimestampAndJavaVersion(count, defLastUptateTime, JkJavaVersion.ofCurrent());
-    }
-
-    private CountTimestampAndJavaVersion stateFromFlagFile() {
-        Path work = projectBaseDir.resolve(JkConstants.WORK_PATH);
-        if (!Files.exists(work)) {
-            return new CountTimestampAndJavaVersion(0, 0L, JkJavaVersion.ofCurrent());
-        }
-        Path lastUpdateFile = work.resolve(LAST_UPDATE_FILE_NAME);
-        if (!Files.exists(lastUpdateFile)) {
-            return new CountTimestampAndJavaVersion(0, 0L, JkJavaVersion.ofCurrent());
-        }
-        JkLog.trace("Read compilation flag file " + lastUpdateFile);
-        try {
-            String content = JkUtilsPath.readAllLines(lastUpdateFile).get(0);
-            String[] items = content.split(";");
-            if (items.length != 3) {
-                return new CountTimestampAndJavaVersion(0, 0L, JkJavaVersion.ofCurrent());
-            }
-            return new CountTimestampAndJavaVersion(Integer.parseInt(items[0]), Long.parseLong(items[1]),
-                    JkJavaVersion.of(items[2]));
-        } catch (RuntimeException e) {
-            JkLog.warn("Error caught when reading file content of " + lastUpdateFile + ". " + e.getMessage() );
-            return new CountTimestampAndJavaVersion(0,0L, JkJavaVersion.ofCurrent());
-        }
-    }
-
-    private int fileCount() {
-        return JkPathTree.of(projectBaseDir.toAbsolutePath().resolve(JkConstants.DEF_DIR)).count(1000, true) +
-                JkPathTree.of(projectBaseDir.toAbsolutePath().resolve(JkConstants.BOOT_DIR)).count(1000,true);
-    }
 
     boolean isMissingBinaryFiles() {
         Path work = projectBaseDir.resolve(JkConstants.DEF_BIN_DIR);
@@ -126,49 +83,5 @@ class EngineCompilationUpdateTracker {
                 JkPathTree.of(def).count(Integer.MAX_VALUE, false);
     }
 
-    private static class CountTimestampAndJavaVersion {
 
-        final int fileCount;
-
-        final long timestamp;
-
-        final JkJavaVersion javaVersion;
-
-        public CountTimestampAndJavaVersion(int fileCount, long timestamp, JkJavaVersion javaVersion) {
-            this.fileCount = fileCount;
-            this.timestamp = timestamp;
-            this.javaVersion = javaVersion;
-        }
-
-        @Override
-        public String toString() {
-            return "{" +
-                    "fileCount=" + fileCount +
-                    ", timestamp=" + timestamp +
-                    ", javaVersion=" + javaVersion +
-                    '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            CountTimestampAndJavaVersion that = (CountTimestampAndJavaVersion) o;
-
-            if (fileCount != that.fileCount) return false;
-            if (timestamp != that.timestamp) return false;
-            if (!javaVersion.equals(that.javaVersion)) return false;
-
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = fileCount;
-            result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
-            result = 31 * result + javaVersion.hashCode();
-            return result;
-        }
-    }
 }
