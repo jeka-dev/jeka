99e21eb14ebf252217d54c84beff2d4d0995d5bc
djeang <djeangdev@yahoo.fr>
2024-02-13 21:58:49 +0100

    Rework logging

--- dev.jeka.core/src/main/java/dev/jeka/core/tool/FieldInjector.java
+++ dev.jeka.core/src/main/java/dev/jeka/core/tool/FieldInjector.java
@@ -32,7 +32,7 @@ final class FieldInjector {
     }
 
     static void injectAnnotatedProperties(Object target, JkProperties properties) {
-        JkLog.verboseStartTask("Injecting JkProperties values into %s", target);
+        JkLog.debugStartTask("Injecting JkProperties values into %s", target);
         for (final Field field : getPropertyFields(target.getClass())) {
             final JkInjectProperty injectProperty = field.getAnnotation(JkInjectProperty.class);
             if (injectProperty == null) {
@@ -50,23 +50,23 @@ final class FieldInjector {
                         throw new JkException("Property " + injectProperty.value() + " has been set with improper value '"
                                 + propertyValue + "' : " + e.getMessage());
                     }
-                    JkLog.verbose("Inject property value %s in field %s.", value, field);
+                    JkLog.debug("Inject property value %s in field %s.", value, field);
                     JkUtilsReflect.setFieldValue(target, field, value);
                 } else {
-                    JkLog.verbose("No property %s declared to inject in field %s.", injectedPropName, field);
+                    JkLog.debug("No property %s declared to inject in field %s.", injectedPropName, field);
                 }
 
                 // We explore nested field only if a naked @JKInjectProperty is present on the parent field.
                 // This is to avoid exploring on deep trees with potential recursive issues (as found on JkProject on ProjectKBean)
             } else {
-                JkLog.verbose("Naked @JkInjectProperty found on field %s. Exploring nested fields for injection", field);
+                JkLog.debug("Naked @JkInjectProperty found on field %s. Exploring nested fields for injection", field);
                 Object fieldValue = JkUtilsReflect.getFieldValue(target, field);
                 if (fieldValue != null) {
                     injectAnnotatedProperties(fieldValue, properties);
                 }
             }
         }
-        JkLog.verboseEndTask();
+        JkLog.debugEndTask();
     }
 
     static List<Field> getPropertyFields(Class<?> clazz) {
